c6ef4f8358d85fb50473e7e362b20bce909927ff
John Blyberg <john@blyberg.net>
2025-11-15 13:13:08 -0500

    Yazi git integration

--- shared/yazi/.config/yazi/init.lua
+++ shared/yazi/.config/yazi/init.lua
@@ -72,3 +72,52 @@ Status:children_add(function()
     " ",
   }
 end, 500, Status.RIGHT)
+
+-- th.git = th.git or {}
+-- th.git.modified_sign = "M"
+-- th.git.deleted_sign = "D"
+require("git"):setup()
+require("githead"):setup({
+  order = {
+    "__spacer__",
+    "branch",
+    "commit",
+    "__spacer__",
+    "behind_ahead_remote",
+    "__spacer__",
+    "untracked",
+    "state",
+    "unstaged",
+    "__spacer__",
+    "staged",
+  },
+
+  show_numbers = false,
+
+  show_branch = true,
+  branch_prefix = "",
+  branch_color = "#288BD2",
+
+  always_show_commit = true,
+  commit_color = "#859A00",
+
+  show_behind_ahead_remote = true,
+  behind_remote_symbol = "↓",
+  ahead_remote_symbol = "↑",
+  behind_remote_color = "#DC322E",
+  ahead_remote_color = "#4DB6AC",
+
+  show_state = true,
+  show_state_prefix = false,
+  state_symbol = "!!",
+  state_color = "#B58901",
+
+  staged_symbol = "✔",
+  staged_color = "green",
+
+  unstaged_symbol = "Δ",
+  unstaged_color = "#288BD2",
+
+  untracked_symbol = "?",
+  untracked_color = "#415F65",
+})
