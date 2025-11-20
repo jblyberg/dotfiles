410547962d901c188c7c47d69cc1cd9ef8eb4515
John Blyberg <john@blyberg.net>
2025-11-20 09:04:54 -0500

    Add moor as pager

--- shared/fish/.config/fish/config.fish
+++ shared/fish/.config/fish/config.fish
@@ -11,6 +11,7 @@ if status is-interactive
   # Enviroment variables
   export EDITOR=nvim
   export MANPAGER="sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat -p -lman'"
+  export PAGER="moor"
 
   # Paths
   fish_add_path ~/.local/bin
