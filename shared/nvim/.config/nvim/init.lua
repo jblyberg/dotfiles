788862888aee5ba3b3f627137f268652139d446f
John Blyberg <john@blyberg.net>
2026-01-05 23:41:25 -0500

    Neovim - dotnet

--- shared/nvim/.config/nvim/init.lua
+++ shared/nvim/.config/nvim/init.lua
@@ -1,3 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 -- require("config.lsp")
 require("config.lazy")
+
+local projectfile = vim.fn.getcwd() .. 'project.godot'
+if projectfile then
+  vim.fn.serverstart './godothost'
+end
