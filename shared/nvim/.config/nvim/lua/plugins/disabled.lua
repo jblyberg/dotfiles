56c9d57546c25b16526fe1ab59252bf7a13d2c66
John Blyberg <john@blyberg.net>
2026-01-13 10:54:06 -0500

    Disable neo-tree

--- shared/nvim/.config/nvim/lua/plugins/disabled.lua
+++ shared/nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
   { "goolord/alpha-nvim",                      enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim",             enabled = false },
   { "nvim-treesitter/nvim-treesitter-context", enabled = false },
 }
