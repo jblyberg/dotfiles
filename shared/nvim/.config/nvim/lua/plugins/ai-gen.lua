5f28d955aaf0e4d9435374169efd60e3598f34e1
John Blyberg <john@blyberg.net>
2026-01-14 15:55:19 -0500

    Add AI integration to neovim

---
+++ shared/nvim/.config/nvim/lua/plugins/ai-gen.lua
@@ -0,0 +1,12 @@
+return {
+  "David-Kunz/gen.nvim",
+  opts = {
+    model = "mistral:7b",
+    host = "maquoit.home.blyberg.net",
+    port = "11434",
+    display_mode = "float",
+    show_prompt = false,
+    show_model = true,
+    no_auto_close = false,
+  }
+}
