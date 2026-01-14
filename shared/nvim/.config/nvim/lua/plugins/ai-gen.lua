af074db0d037e99ea03617d1f47c8f374eae153a
John Blyberg <john@blyberg.net>
2026-01-14 16:11:12 -0500

    Refine ollama config

--- shared/nvim/.config/nvim/lua/plugins/ai-gen.lua
+++ shared/nvim/.config/nvim/lua/plugins/ai-gen.lua
@@ -1,12 +1,16 @@
 return {
   "David-Kunz/gen.nvim",
-  opts = {
-    model = "mistral:7b",
-    host = "maquoit.home.blyberg.net",
-    port = "11434",
-    display_mode = "float",
-    show_prompt = false,
-    show_model = true,
-    no_auto_close = false,
-  }
+  opts = function(_, opts)
+    vim.keymap.set({ 'n', 'v' }, '<leader>]', ':Gen<CR>')
+
+    return {
+      model = "mistral:7b",
+      host = "maquoit.home.blyberg.net",
+      port = "11434",
+      display_mode = "float",
+      show_prompt = false,
+      show_model = true,
+      no_auto_close = false,
+    }
+  end
 }
