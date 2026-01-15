23cace695f9df65e7d7b4bb22d8064f45873da88
John Blyberg <john@blyberg.net>
2026-01-15 18:02:22 -0500

    Clean up nvim plugins

--- shared/nvim/.config/nvim/lua/plugins/disabled.lua
+++ shared/nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,9 @@
 return {
+  { "ThePrimeagen/harpoon",                    enabled = false },
+  { "barrett-ruth/import-cost.nvim",           enabled = false },
   { "goolord/alpha-nvim",                      enabled = false },
+  { "jackMort/ChatGPT.nvim",                   enabled = false },
+  { "kylechui/nvim-surround",                  enabled = false },
   { "nvim-neo-tree/neo-tree.nvim",             enabled = false },
   { "nvim-treesitter/nvim-treesitter-context", enabled = false },
 }
--- shared/nvim/.config/nvim/lua/plugins/import-cost.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"barrett-ruth/import-cost.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	build = "sh install.sh yarn",
-	config = function()
-		require("import-cost").setup({})
-	end,
-}
