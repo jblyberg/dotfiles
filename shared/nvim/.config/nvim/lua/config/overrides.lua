dc2926ac29ada08445406bfbf718eb22b11f6726
John Blyberg <john@blyberg.net>
2026-01-18 14:43:37 -0500

    Neovim config tweaks. DMS.

--- shared/nvim/.config/nvim/lua/config/overrides.lua
+++ shared/nvim/.config/nvim/lua/config/overrides.lua
@@ -1,12 +1,15 @@
-local banned_messages = {
-  "No information available"
+local original_notify = vim.notify
+
+local banned_patterns = {
+  "No information available",
+  -- "^LSP%[.*%]", -- Filter LSP messages
 }
 
 vim.notify = function(msg, ...)
-  for _, banned in ipairs(banned_messages) do
-    if msg == banned then
+  for _, pattern in ipairs(banned_patterns) do
+    if string.find(msg, pattern) then
       return
     end
   end
-  require("notify")(msg, ...)
+  original_notify(msg, ...)
 end
